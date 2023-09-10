1d11cf95801130b5c589abe4c7a275c67be5798a
Joshua <jhoang304@gmail.com>
2023-09-10 03:04:35 -0500

    added routes for profile page

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import User
+from app.models import User, db
+from app.forms import UserProfileForm
+from app.api.auth_routes import authenticate
 
 user_routes = Blueprint('users', __name__)
 
@@ -23,3 +25,26 @@ def user(id):
     """
     user = User.query.get(id)
     return user.to_dict()
+
+@user_routes.route('/get/<int:id>', methods=['GET'])
+def get_user_profile(id):
+    profile = User.query.filter(User.id == id).all()
+    return profile[0].to_dict()
+
+
+@user_routes.route('/<int:id>/edit', methods=['PUT'])
+@login_required
+def edit_profile(id):
+    if authenticate():
+        form = UserProfileForm()
+        profile = User.query.get(id)
+        if not profile:
+            return {'errors': ['The profile does not exist']}, 401
+        form['csrf_token'].data = request.cookies['csrf_token']
+        data = form.data
+
+        if profile and form.validate_on_submit():
+            profile.username = data['username']
+            db.session.commit()
+            return profile.to_dict()
+        return form.errors
