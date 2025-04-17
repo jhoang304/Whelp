c9f8679c12f00678961c5bfea5b322ae480c4650
Joshua <jhoang304@gmail.com>
2025-04-17 17:52:07 -0500

    added styling to login page

--- react-app/src/components/LoginFormPage/index.js
+++ react-app/src/components/LoginFormPage/index.js
@@ -18,46 +18,73 @@ function LoginFormPage() {
     const data = await dispatch(login(email_address, password));
     if (data) {
       setErrors(data);
+    } else {
+      setErrors([]); // Clear errors on successful login
     }
   };
 
+  const handleDemoLogin = () => {
+    dispatch(login('demo@aa.io', 'password')).then((data) => {
+      if (data) {
+        setErrors(data);
+      } else {
+        setErrors([]);
+      }
+    });
+  };
+
   return (
-    <>
-      <h1 className="logintext">Log In to Whelp</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="errors">
-          {errors.map((error, idx) => (
-            <div className="login-error" key={idx}>{error}</div>
-          ))}
+    <div className="login-page-container">
+      <div className="login-form-container">
+        <div className="login-image-section">
+          <img src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="Restaurant food" />
         </div>
-        <div className="autharea">
-          <div className="authdeets">
+        <div className="login-form-section">
+          <h1 className="login-title">Log In to Whelp</h1>
+          <p className="login-subtitle">Access your account</p>
+          <form onSubmit={handleSubmit} className="login-form">
+            {errors.length > 0 && (
+              <div className="login-errors-container">
+                {errors.map((error, idx) => (
+                  <div className="login-error-message" key={idx}>{error}</div>
+                ))}
+              </div>
+            )}
+            <div className="input-group">
+              <label htmlFor="email">Email Address</label>
               <input
-                className="inputdeet"
-                placeholder="Email"
+                id="email"
+                className="login-input"
+                placeholder="Enter your email"
                 type="text"
                 value={email_address}
                 onChange={(e) => setEmail_Address(e.target.value)}
                 required
               />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
               <input
-                className="inputdeet"
+                id="password"
+                className="login-input"
                 type="password"
-                placeholder="Password"
+                placeholder="Enter your password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
               />
-            <button type="button" className="sibutton2" onClick={() => {dispatch(login('demo@aa.io', 'password'))}}>Log in as a Demo User</button>
-            <button className="sibutton2" type="submit">Log In</button>
-          </div>
-          <div className="redcarpetpic">
-            <img src="https://s3-media0.fl.yelpcdn.com/assets/2/www/img/7922e77f338d/signup/signup_illustration.png" alt=""></img>
-          </div>
+            </div>
+            <button className="login-submit-button" type="submit">Log In</button>
+            <button type="button" className="demo-login-button" onClick={handleDemoLogin}>
+              Log in as Demo User
+            </button>
+          </form>
+          <p className="signup-link">
+            Don't have an account?  <a href="/signup">Sign Up</a>
+          </p>
         </div>
-      </form>
-
-    </>
+      </div>
+    </div>
   );
 }
 
